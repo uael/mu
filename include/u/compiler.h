bcd5598325fe676ed13d629873a8640c4b11439c
uael <lucas.abel@yandex.com>
2017-05-25 14:42:33 +0200

    mu compiler features detections

--- include/u/compiler.h
+++ include/u/compiler.h
@@ -112,23 +112,293 @@
 #define STRINGIFY(x) NX_STRINGIFY(x)
 #define VERSION_NO(A, B, C) (((A)*10000UL)+(B)*100UL+(C))
 
+#ifndef __has_builtin
+#  define __has_builtin(X) u_has_builtin_ ## X
+#endif
+#ifndef __has_feature
+#  define __has_feature(X) u_has_feature_ ## X
+#endif
+#ifndef __has_extension
+#  define __has_extension __has_feature
+#endif
+#ifndef __has_attribute
+#  define __has_attribute(X) u_has_attribute_ ## X
+#endif
+
+#ifndef HAS_BUILTIN
+# define HAS_BUILTIN(X) (__has_builtin(X) || u_has_builtin_ ## X)
+# define HAS_FEATURE(X) (__has_feature(X) || u_has_feature_ ## X)
+# define HAS_EXTENSION(X) (__has_extension(X) || u_has_extension_ ## X)
+# define HAS_ATTRIBUTE(X) (__has_attribute(X) || u_has_attribute_ ## X)
+#endif
+
+#if __STDC_VERSION__ > 201100L
+# ifndef u_has_feature_stdnoreturn_h
+#   define u_has_feature_stdnoreturn_h 1
+# endif
+# ifndef u_has_feature_c_max_align_t
+#   define u_has_feature_c_max_align_t 1
+# endif
+# ifndef u_has_feature_c_generic_selections
+#   define u_has_feature_c_generic_selections 1
+# endif
+#endif
+
+#if HAS_BUILTIN(__sync_val_compare_and_swap)
+# ifndef __GCC_HAVE_SYNC_COMPARE_AND_SWAP_1
+#   define __GCC_HAVE_SYNC_COMPARE_AND_SWAP_1 1
+# endif
+# ifndef __GCC_HAVE_SYNC_COMPARE_AND_SWAP_2
+#   define __GCC_HAVE_SYNC_COMPARE_AND_SWAP_2 1
+# endif
+# ifndef __GCC_HAVE_SYNC_COMPARE_AND_SWAP_4
+#   define __GCC_HAVE_SYNC_COMPARE_AND_SWAP_4 1
+#   if !defined(__GCC_HAVE_SYNC_COMPARE_AND_SWAP_8) && (UINTPTR_MAX >= UINT64_MAX)
+#     define __GCC_HAVE_SYNC_COMPARE_AND_SWAP_8 1
+#   endif
+# endif
+#endif
+
 #if defined(__clang__)
 # undef  COMPILER_CLANG
 # define COMPILER_CLANG 1
 # define COMPILER_NAME "clang"
 # define COMPILER_DESCRIPTION COMPILER_NAME " " #__clang_major__ "." #__clang_minor__
 # define COMPILER_VERSION VERSION_NO(__clang_major__, __clang_minor__, __clang_patchlevel__)
-# define restrict __restrict
+# if COMPILER_VERSION > 30200UL
+#   define u_has_feature_stdnoreturn_h 1
+# endif
+#elif defined(_MSC_VER)
+# undef  COMPILER_MSVC
+# define COMPILER_MSVC 1
+# define COMPILER_NAME "msvc"
+# define COMPILER_DESCRIPTION COMPILER_NAME " " STRINGIFY(_MSC_VER)
+# define COMPILER_VERSION _MSC_VER
+#elif defined(__GNUC__)
+# undef  COMPILER_GCC
+# define COMPILER_GCC 1
+# define COMPILER_NAME "gcc"
+# define COMPILER_DESCRIPTION COMPILER_NAME " " STRINGIFY(__GNUC__) "." STRINGIFY(__GNUC_MINOR__)
+# define COMPILER_VERSION VERSION_NO(__GNUC__, __GNUC_MINOR__, __GNUC_PATCHLEVEL__)
+# define u_has_attribute_always_inline 1
+# define u_has_attribute_noinline 1
+# define u_has_attribute_weak 1
+# define u_has_attribute_weakref 1
+# define u_has_attribute_constructor 1
+# define u_has_attribute_destructor 1
+# define u_has_attribute_vector_size 1
+# define u_has_attribute_warn_unused_result 1
+# define u_has_extension_attribute_const 1
+# define u_has_attribute_pure 1
+# if defined(__GNUC_GNU_INLINE__) || (COMPILER_VERSION < 40300UL)
+#  define u_has_attribute_gnu_inline 1
+# endif
+# define u_has_attribute_aligned 1
+# define u_has_attribute_noreturn 1
+# define u_has_attribute___noreturn__ 1
+# define u_has_attribute_deprecated 1
+# define u_has_attribute_unused 1
+# ifdef __GCC_HAVE_SYNC_COMPARE_AND_SWAP_4
+#  define u_has_builtin___sync_val_compare_and_swap 1
+#  define u_has_builtin___sync_lock_test_and_set 1
+#  define u_has_builtin___sync_lock_release 1
+#  define u_has_builtin___sync_synchronize 1
+#  define u_has_builtin___sync_fetch_and_add 1
+#  define u_has_builtin___sync_fetch_and_sub 1
+#  define u_has_builtin___sync_fetch_and_or 1
+#  define u_has_builtin___sync_fetch_and_and 1
+#  define u_has_builtin___sync_fetch_and_xor 1
+# endif
+# if COMPILER_VERSION >= 30000UL
+#  define u_has_builtin___builtin_expect 1
+# endif
+# if COMPILER_VERSION >= 40300UL
+#  define u_has_attribute_error 1
+# endif
+# if COMPILER_VERSION >= 40600UL
+#  define u_has_feature_c_static_assert 1
+#  define u_has_attribute_externally_visible 1
+# endif
+# if defined(__GNUC_STDC_INLINE__) || COMPILER_VERSION >= 40300UL
+#  define u_has_feature_c_inline 1
+# endif
+# define u_has_feature_gnu_thread_local 1
+# define u_has_feature_gnu_alignof 1
+# define u_has_feature_statement_expression 1
+# define u_has_feature_tgmath_h 1
+# define TYPEOF(X) __typeof__(X)
+# if (COMPILER_VERSION >= 40700UL) && (COMPILER_VERSION < 40900UL)
+#  define u_has_feature_stdnoreturn_h 1
+#  define u_has_feature_stdalign_h 1
+#  if __STDC_VERSION__ > 201100L
+#   define u_has_feature_c_max_align_t 1
+#   define __STDC_NO_ATOMICS__ 1
+#   define u_has_feature_c_generic_selections 0
+#  endif
+# endif
+# if (COMPILER_VERSION >= 40900UL) && (COMPILER_VERSION < 41000UL)
+#  define u_has_feature_stdnoreturn_h 1
+#  define u_has_feature_stdalign_h 1
+#  if __STDC_VERSION__ > 201100L
+#   define u_has_feature_c_max_align_t 1
+#   define u_has_feature_c_generic_selections 1
+#  endif
+# endif
+#elif defined(__ICL) || defined(__ICC) || defined(__INTEL_COMPILER)
+# undef  COMPILER_INTEL
+# define COMPILER_INTEL 1
+# define COMPILER_NAME "intel"
+# if defined(__ICL)
+#   define COMPILER_DESCRIPTION COMPILER_NAME " " #__ICL
+# elif defined(__ICC)
+#   define COMPILER_DESCRIPTION COMPILER_NAME " " #__ICC
+# endif
+# define COMPILER_VERSION VERSION_NO(__INTEL_COMPILER, 0, 0)
+# if __INTEL_COMPILER < 1310
+#   if defined(__GNUC__) && defined(u_has_feature_tgmath_h)
+#     undef u_has_feature_tgmath_h
+#   endif
+# else
+#   define u_has_feature_stdatomic_h 1
+# endif
+#else
+# warning Unknown compiler
+# define COMPILER_NAME "unknown"
+# define COMPILER_DESCRIPTION "unknown"
+#endif
+
+#if PLATFORM_POSIX
+# ifndef _GNU_SOURCE
+#   define _GNU_SOURCE
+# endif
+#endif
+
+#if COMPILER_INTEL
+# if (__ICC < 1400) && (__GNUC__ == 4) && (__GNUC_MINOR__ > 2)
+#   undef __GNUC_MINOR__
+#   define __GNUC_MINOR__ 2
+#   undef __GNUC_PATCHLEVEL__
+#   define __GNUC_PATCHLEVEL__ 0
+# endif
+#endif
+
+#ifdef __GNUC__
+# define extension __extension__
+#endif
+#ifndef extension
+# define extension
+#endif
+
+#define restrict __restrict
+
+#if COMPILER_MSVC
+# define thread_local __declspec(thread)
+#elif COMPILER_GCC
+# define thread_local __thread
+#else
 # define thread_local _Thread_local
-# define ATTR(x) __attribute__((__##x##__))
-# define ATTR2(x,y) __attribute__((__##x##__(y)))
-# define ATTR3(x,y,z) __attribute__((__##x##__(y,z)))
-# define ATTR4(x,y,z,w) __attribute__((__##x##__(y,z,w)))
-# define DEPRECATED ATTR(deprecated)
-# define FORCEINLINE inline ATTR(always_inline)
-# define NOINLINE ATTR(noinline)
-# define PURECALL ATTR(pure)
-# define CONSTCALL ATTR(const)
+#endif
+
+#if COMPILER_GCC && (__GNUC__ >= 3 || (__GNUC__ == 2 && __GNUC_MINOR__ >= 70))
+# define NORETURN __attribute__((__noreturn__))
+#elif defined(__STDC__) && __STDC_VERSION__ >= 201112L
+# define NORETURN _Noreturn
+#elif COMPILER_WINDOWS
+# define NORETURN __declspec(noreturn)
+#else
+# define NORETURN
+#endif
+
+#if HAS_ATTRIBUTE(deprecated)
+# define DEPRECATED __attribute__((__deprecated__))
+#elif COMPILER_MSVC
+# define DEPRECATED __declspec(deprecated)
+#else
+# define DEPRECATED
+#endif
+
+#if HAS_FEATURE(c_inline)
+# define inline __inline__
+# define FORCEINLINE __attribute__((__always_inline__)) __inline__
+# if HAS_ATTRIBUTE(externally_visible)
+#   define INSTANTIATE __attribute__((__externally_visible__)) extern __inline__
+# else
+#   define INSTANTIATE extern __inline__
+# endif
+#elif COMPILER_GCC
+# ifdef inline
+#   undef inline
+# endif
+# if HAS_ATTRIBUTE(gnu_inline)
+#   define inline __attribute__((__gnu_inline__,__weak__)) __inline__
+#   define FORCEINLINE __attribute__((__always_inline__,__gnu_inline__,__weak__)) __inline__
+# else
+#   define inline __attribute__((__weak__)) __inline__
+#   define FORCEINLINE __attribute__((__always_inline__,__weak__)) __inline__
+# endif
+# define static_inline static __inline__
+# if HAS_ATTRIBUTE(externally_visible)
+#   define INSTANTIATE __attribute__((__externally_visible__))
+# else
+#   define INSTANTIATE
+# endif
+#elif HAS_ATTRIBUTE(always_inline)
+# define FORCEINLINE __attribute__(__always_inline__) inline
+#elif COMPILER_MSVC || COMPILER_INTEL
+# define FORCEINLINE __forceinline
+#else
+# define FORCEINLINE static inline
+#endif
+
+#if HAS_ATTRIBUTE(noinline)
+# define NOINLINE __attribute__((__noinline__))
+#elif COMPILER_MSVC || COMPILER_INTEL
+# define NOINLINE __declspec(noinline)
+#else
+# define NOINLINE
+#endif
+
+#if HAS_ATTRIBUTE(pure)
+# define PURECALL __attribute__((__pure__))
+#else
+# define PURECALL
+#endif
+
+#if HAS_EXTENSION(attribute_const)
+# define CONSTCALL __attribute__((__const__))
+#else
+# define CONSTCALL
+#endif
+
+#ifndef INSTANTIATE
+# if HAS_ATTRIBUTE(externally_visible)
+#   define INSTANTIATE __attribute__((__externally_visible__)) extern __inline__
+# else
+#   define INSTANTIATE extern __inline__
+# endif
+#endif
+
+#if HAS_ATTRIBUTE(weak)
+# define WEAK(ID) __attribute__((__weak__))
+#elif COMPILER_MSVC
+# define WEAK(ID) __declspec(selectany)
+#else
+# define WEAK(ID) _Pragma(STRINGIFY(weak ID))
+#endif
+
+#if HAS_ATTRIBUTE(weakref)
+# define WEAKREF(ID, ...) __attribute__((__weakref__(#__VA_ARGS__)))
+#else
+# define WEAKREF(ID, ...) _Pragma(STRINGIFY(weak ID=__VA_ARGS__))
+#endif
+
+#if HAS_ATTRIBUTE(warn_unused_result)
+# define UNUSED_RESULT __attribute__((__warn_unused_result__))
+#else
+# define UNUSED_RESULT
+#endif
+
+#if COMPILER_CLANG
 # define PRINTFCALL(start, num) ATTR4(format, printf, start, num)
 # define ALIGN(alignment) ATTR2(aligned, alignment)
 # define alignof(type) __alignof__(type)
@@ -166,27 +436,7 @@
 # if __has_warning("-Wcovered-switch-default")
 #   pragma clang diagnostic ignored "-Wcovered-switch-default"
 # endif
-#elif defined(__ICL) || defined(__ICC) || defined(__INTEL_COMPILER)
-# undef  COMPILER_INTEL
-# define COMPILER_INTEL 1
-# define COMPILER_NAME "intel"
-# if defined(__ICL)
-#   define COMPILER_DESCRIPTION COMPILER_NAME " " #__ICL
-# elif defined(__ICC)
-#   define COMPILER_DESCRIPTION COMPILER_NAME " " #__ICC
-# endif
-# define COMPILER_VERSION VERSION_NO(__INTEL_COMPILER, 0, 0)
-# define restrict __restrict
-# define thread_local __declspec(thread)
-# define ATTR(x)
-# define ATTR2(x,y)
-# define ATTR3(x,y,z)
-# define ATTR4(x,y,z,w)
-# define DEPRECATED
-# define FORCEINLINE __forceinline
-# define NOINLINE __declspec(noinline)
-# define PURECALL
-# define CONSTCALL
+#elif COMPILER_INTEL
 # define PRINTFCALL(start, num)
 # define ALIGN(alignment) __declspec(align(alignment))
 # define alignof(type) __alignof(type)
@@ -198,24 +448,7 @@
 #   define va_copy(d,s) ((d)=(s))
 # endif
 # include <intrin.h>
-#elif defined(_MSC_VER)
-# undef  COMPILER_MSVC
-# define COMPILER_MSVC 1
-# define COMPILER_NAME "msvc"
-# define COMPILER_DESCRIPTION COMPILER_NAME " " STRINGIFY(_MSC_VER)
-# define COMPILER_VERSION _MSC_VER
-# define ATTR(x)
-# define ATTR2(x,y)
-# define ATTR3(x,y,z)
-# define ATTR4(x,y,z,w)
-# define restrict __restrict
-# define thread_local __declspec(thread)
-# define DEPRECATED __declspec(deprecated)
-# define FORCEINLINE __forceinline
-# define NOINLINE __declspec(noinline)
-# define NORETURN __declspec(noreturn)
-# define PURECALL
-# define CONSTCALL
+#elif COMPILER_MSVC
 # define PRINTFCALL(start, num)
 # define ALIGN(alignment) __declspec(align(alignment))
 # define alignof(type) __alignof(type)
@@ -248,23 +481,7 @@
 #   define va_copy(d,s) ((d)=(s))
 # endif
 # include <intrin.h>
-#elif defined(__GNUC__)
-# undef  COMPILER_GCC
-# define COMPILER_GCC 1
-# define COMPILER_NAME "gcc"
-# define COMPILER_DESCRIPTION COMPILER_NAME " " STRINGIFY(__GNUC__) "." STRINGIFY(__GNUC_MINOR__)
-# define COMPILER_VERSION VERSION_NO(__GNUC__, __GNUC_MINOR__, __GNUC_PATCHLEVEL__)
-# define restrict __restrict
-# define thread_local __thread
-# define ATTR(x) __attribute__((__##x##__))
-# define ATTR2(x,y) __attribute__((__##x##__(y)))
-# define ATTR3(x,y,z) __attribute__((__##x##__(y,z)))
-# define ATTR4(x,y,z,w) __attribute__((__##x##__(y,z,w)))
-# define DEPRECATED ATTR(deprecated)
-# define FORCEINLINE inline ATTR(always_inline)
-# define NOINLINE ATTR(noinline)
-# define PURECALL ATTR(pure)
-# define CONSTCALL ATTR(const)
+#elif COMPILER_GCC
 # define PRINTFCALL(start, num) ATTR4(format, printf, start, num)
 # define ALIGN(alignment) ATTR2(aligned, alignment)
 # define alignof(type) __alignof__(type)
@@ -292,22 +509,6 @@
 #   pragma GCC diagnostic ignored "-Wformat-extra-args"
 #   pragma GCC diagnostic ignored "-Wpedantic"
 # endif
-#else
-# warning Unknown compiler
-# define COMPILER_NAME "unknown"
-# define COMPILER_DESCRIPTION "unknown"
-# define restrict
-# define thread_local
-# define DEPRECATED
-# define FORCEINLINE
-# define NOINLINE
-# define PURECALL
-# define CONSTCALL
-# define ALIGN
-# define alignof
-# define ALIGNED_STRUCT(name, alignment) struct name
-# define LIKELY(x) (x)
-# define UNLIKELY(x) (x)
 #endif
 
 #ifndef static_assert
@@ -318,12 +519,6 @@
      "static assertion failed: " STRINGIFY(EXPR) ", " DIAGSTR}) \
   ]
   extern char const u_compiletime_assert[sizeof(void const*[2])];
-#endif
-
-#if PLATFORM_POSIX
-# ifndef _GNU_SOURCE
-#   define _GNU_SOURCE
-# endif
 #endif
 
 #endif /* U_COMPILER_H__ */
