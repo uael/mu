00fed247c3f18ea39e939650ff597ef44d72e14d
uael <lucas.abel@yandex.com>
2017-06-07 12:11:34 +0200

    updated cute

--- vendor/cute/include/cute.h
+++ vendor/cute/include/cute.h
@@ -42,14 +42,12 @@
 # define CUTE_CONSTCALL
 #endif
 
-#ifdef __cplusplus
-# define CUTE_UNUSED [[unused]]
-#elif defined(__GNUC__) || defined(__clang__)
+#if defined(__GNUC__) || defined(__clang__)
 # define CUTE_UNUSED __attribute__((unused))
 #elif defined(__LCLINT__)
 # define CUTE_UNUSED /*@unused@*/
 #else
-# define CUTE_UNUSED x
+# define CUTE_UNUSED
 #endif
 
 #if defined(_WIN32) || defined(__WIN32__) || defined(_WIN64)
