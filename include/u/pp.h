a0b9a8f5e3a5e71220dcb47585867b045825db14
uael <lucas.abel@yandex.com>
2017-06-08 08:28:28 +0200

    fixed PP_MCALL on msvc compiler

--- include/u/pp.h
+++ include/u/pp.h
@@ -32,17 +32,17 @@
 #define PP_VA_ARGS(...) __VA_ARGS__
 #define PP_EVAL(...) PP_VA_ARGS(PP_VA_ARGS(PP_VA_ARGS(__VA_ARGS__)))
 
+#define PP_PRIMITIVE_JOIN__(x, y) x##y
+#define PP_PRIMITIVE_JOIN(x, y) PP_PRIMITIVE_JOIN__(x, y)
+#define PP_JOIN(x, y) PP_PRIMITIVE_JOIN(x, y)
+
 #define PP_LPAR (
 #define PP_RPAR )
 #define PP_VA_PASS(...) PP_LPAR __VA_ARGS__ PP_RPAR
-#define PP_MCALL(macro, ...) PP_EVAL(PP_EVAL(macro) PP_VA_PASS(__VA_ARGS__))
+#define PP_MCALL(macro, ...) PP_EVAL(PP_JOIN(PP_EVAL(macro), PP_VA_PASS(__VA_ARGS__)))
 
 #define PP_VA_NARGS_PEEK(_1, _2, _3, _4, _5, _6, _7, _8, _9, N, ...) N
 #define PP_VA_NARGS_RSEQ 9, 8, 7, 6, 5, 4, 3, 2, 1
 #define PP_VA_NARGS(...) PP_MCALL(PP_VA_NARGS_PEEK, __VA_ARGS__, PP_VA_NARGS_RSEQ)
-
-#define PP_PRIMITIVE_JOIN__(x, y) x##y
-#define PP_PRIMITIVE_JOIN(x, y) PP_PRIMITIVE_JOIN__(x, y)
-#define PP_JOIN(x, y) PP_PRIMITIVE_JOIN(x, y)
 
 #endif /* U_PP_H__ */
